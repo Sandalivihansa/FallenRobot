16c630c60fb14044828df4aa7872570261eb25bd
Sandalivihansa <114420339+Sandalivihansa@users.noreply.github.com>
2024-12-26 20:27:46 -0800

    Update config.py
    Signed-off-by: Sandalivihansa <114420339+Sandalivihansa@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -18,7 +18,7 @@ class Config(object):
 
     SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "7332398186:AAHG5L3MF-8BtP4ouR_9a_T2tBgje_GegN0"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
